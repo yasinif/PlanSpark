ee053057a4b88ee284f2c843c4ed9b3fcb9f61a8
yasinif <yasin.pc81@gmail.com>
2026-07-26 06:43:33 +0330

    fix: move db init outside of main block

--- app.py
+++ app.py
@@ -951,8 +951,8 @@ def create_app() -> Flask:
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    
     app.run(host="0.0.0.0", port=5000, debug=os.environ.get("FLASK_DEBUG", "0") == "1")
